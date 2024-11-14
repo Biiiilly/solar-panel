de5cca71531de28bd37ec80cc3500109c75ae5d5
Biiiilly <bm221@ic.ac.uk>
2024-11-14 16:46:47 +0000

    plot

--- plot.py
+++ plot.py
@@ -16,6 +16,17 @@ def plot_data(site_num, date):
     # Filter the data for the date 2019-03-01
     site_data_date = site_data[site_data['timestamp_utc'].dt.date == pd.to_datetime(date).date()]
 
+    plt.figure(figsize=(12, 6))
+    plt.plot(site_data_date['timestamp_utc'], site_data_date['solar_gen_kwh'], label=f'Solar Generation (kWh) on {date}')
+
+    # Set plot labels and title
+    plt.xlabel('Time (UTC)')
+    plt.ylabel('Solar Generation (kWh)')
+    plt.title(f'Solar Generation Over Time for Site {site_num}')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
     return site_data_date
 
 
